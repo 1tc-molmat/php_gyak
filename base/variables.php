22152f8fc8ad1fecb3d1af48756f1128afc5393f
1tc-molmat <matuka20070628@gmail.com>
2025-09-04 11:19:29 +0200

    varibales.php kesz

---
+++ base/variables.php
@@ -0,0 +1,18 @@
+<?php
+    /*szükséges programok:
+                    XAMPP(Apache,MySQl)
+                    Git
+                    Github repo
+                    VSC
+     <?php.. ?>
+     Változók:
+
+
+    */
+$name = "Mate";
+$age="18";
+$city="Kunhegyes";
+var_dump($name);
+
+echo "Név: $name <br> Életkor: $age <br> Város: $city";
+?>

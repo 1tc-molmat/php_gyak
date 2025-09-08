1fd2103fe361e889d97e53626679ef92d0a8c92a
1tc-molmat <matuka20070628@gmail.com>
2025-09-08 08:42:59 +0200

    if done

---
+++ structures.php
@@ -0,0 +1,18 @@
+<?php
+/*
+
+   1. if,else,elseif
+   2. switch
+   3.Ciklusok: for,while,do while,foreach
+*/
+
+//Egy számról döntsü el hogy páros vagy páratlan
+   echo "Szia";
+   $number=7;
+
+   if($number %2==0){
+       echo "A szám páros";
+   }else{
+       echo "A szám páratlan";
+   }
+?>

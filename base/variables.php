d1a65475f01b355f105a434dba009705c7329b16
1tc-molmat <matuka20070628@gmail.com>
2025-09-08 08:16:29 +0200

    git clone

--- base/variables.php
+++ base/variables.php
@@ -15,4 +15,42 @@ $city="Kunhegyes";
 var_dump($name);
 
 echo "Név: $name <br> Életkor: $age <br> Város: $city";
+define("PI",3.14);
+//adattípusok : string,integer,float,boolean,array,object,null
+$message = "egy";
+echo "Kiirva:" $message, "értéket. ", "<br>\n";
+echo "Kiirva: $message értéket, <br>\n";
+echo "Kiirva:" {$messagedarab}, "értéket. ", "<br>\n";
+
+
+//git init: helyi repo létrehozása
+//git status: állapot lekérdezése
+//git add . : fájlok hozzáadása a stage-hez
+//git commit -m "első commit": commit létrehozása
+//git remote add origin <url>: távoli repo hozzáadása
+//git push -u origin main: változók feltöltése a távoli repoba
+//git pull origin main: változók lehúzása a távoli repoból
+//git log: commitok listázása
+//git branch -M main: main branch létrehozása
+//git clone <url>: repo klónozása
+//git checkout -b <branch név>: új branch létrehozása és váltás rá
+//git merge <branch név>: branch összeolvasztása a jelenlegi branch-el
+//git diff: változások megtekintése
+//git reset --hard <commit azonosító>: visszaállás egy korábbi commit
+//git config --global user.name "Név": felhasználónév beállítása
+//git config --global user.email : email cím beállítása
+
+/*
+    Ciklusok:
+        for
+        while
+        do while
+        foreach
+       
+     Termary operator:
+        (feltétel) ? érték1 : érték2
+        
+       Tömbök(indexelt,asszociatív,multidimenziós) 
+*/
+
 ?>

fe17cf79c55c2b5e62ae12a924386c966851c6e7
1tc-molmat <matuka20070628@gmail.com>
2025-09-15 08:50:39 +0200

    egyszerübb

--- urlap.php
+++ urlap.php
@@ -1,8 +1,41 @@
 <?php
-//Egy űrlap amibe egy darab név mezőzel és ott van mögött egy submit gomb 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    echo "Hello, " . $name . "!";
-}
+    if (($_SERVER["REQUEST_METHOD"] === "POST") && isset($_POST["name"]) && ($_POST["name"])!=""){
+        //itt dolgozom fel az űrlapot
+        $name = $_POST["name"] ?? "ismeretlen";
+
+        $password = $_POST["password"] ?? "";
+        echo "Hello, $name ($password)";
+    }
+
+    if (($_SERVER["REQUEST_METHOD"] === "GET") && isset($_GET["name"]) && ($_GET["name"])!=""){
+        //itt dolgozom fel az űrlapot
+        $name =htmlspecialchars ($_GET["name"] ?? "ismeretlen");
+        $password = $_GET["password"] ?? "";
+        echo "Hello, $name";
+    }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>űrlapkezelés</h1>
+    
+    <p> <a href="?name=admin">Kattints ide az üdvözlésért admin!</a> </p>
+    
+    <form action="" method="get">
+        <label for="name">Név:</label>
+        <input type="text" name="name" id="name">
+
+        <label for="password">Jelszó:</label>
+        <input type="text" name="password" id="password">
+        <br>
+        <button type="submit">Küld</button>
+    </form>
+</body>
+</html>
